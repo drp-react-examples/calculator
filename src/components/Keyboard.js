24558ef330162ce1834d6539e02c4941e2923308
Paulo Reis <prsc.reis@gmail.com>
2020-05-18 22:11:35 -0300

    Correção leitura tecla com shift

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Button from "./Button";
 
 const KEYS = [
@@ -22,10 +22,32 @@ const KEYS = [
 ];
 
 const Keyboard = (props) => {
+  const { onPress } = props;
+
+  useEffect(() => {
+    const onKeyDown = (e) => {
+      console.log(e);
+
+      console.log(e.key.toUpperCase());
+
+      let key = (e.keyCode === 13 ? "=" : e.key);
+
+      if (KEYS.indexOf(key.toUpperCase()) !== -1) {
+        onPress(key.toUpperCase());
+      }
+    };
+
+    window.addEventListener("keydown", onKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", onKeyDown);
+    };
+  }, [onPress]);
+
   return (
     <div className="keyboard">
       {KEYS.map((key) => (
-        <Button onClick={props.onPress} key={key}>
+        <Button onClick={onPress} key={key}>
           {key}
         </Button>
       ))}
